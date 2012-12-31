877e39505be0843b662c3956f1b4dc3dc87d6dda
prak <prak@prak.(none)>
2012-12-31 20:00:08 +0530

    Added simple socket program

---
+++ IPCs/sockets/simple_socket/client.c
@@ -0,0 +1,14 @@
+/*
+	File    : client.c
+	Brief   : This file is used for making 
+			  connection with the server application.
+	Date    : 31st Dec 2012
+	Author  : Prakash Ranjan
+*/
+
+int main(int argc, char * argv[]){
+	int ret;
+
+	return 0;
+}
+
---
+++ IPCs/sockets/simple_socket/server.c
@@ -0,0 +1,14 @@
+/*
+	File  : server.c
+	Brief : This file is used for accepting the 
+		    connection request from client application.
+	Date  : 31st Dec 2012
+	Author: Prakash Ranjan
+*/
+
+int main(int argc, char * argv[]){
+	int ret;
+
+	return 0;
+}
+
