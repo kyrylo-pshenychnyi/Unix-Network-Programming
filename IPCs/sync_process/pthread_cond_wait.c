677c43ed7d7620ac1dc1a7b0819651ca5ffaf07f
prak <prak@prak.(none)>
2013-01-14 23:23:24 +0530

    Added mutex_conditonal signalling

--- IPCs/sync_process/pthread_cond_wait.c
+++ IPCs/sync_process/pthread_cond_wait.c
@@ -48,33 +48,40 @@ void * thread6 (void * dummy_ptr ){
 
 }
 
-
-
 /* Array of function pointer which will contain the threads function  */
-void (*(thread(void *ptr)))[6] = {thread1,
-								  thread2,
-								  thread3,
-								  thread4,
-								  thread5,
-								  thread6}; 
+	void * (*thread_func[NTHREADS])(void *ptr) = {thread1,
+								         thread2,
+								         thread3,
+								         thread4,
+								         thread5,
+								         thread6}; 
+
+
 
 /* Main function starts here */
-
 int main( int argc , char * argv[]){
+	int ret ;
+	int i;
+	void *dummy_var;
 
-	pthread_t id[NTHREAD];
+	pthread_t id[NTHREADS];
 
-
-	for(i = 0; i < NTHREAD ; i++){
-		ret = pthread_create(&id[i], NULL; func[i] ; NULL);
+	/* Creating threads for communicating through queue */
+	for(i = 0; i < NTHREADS ; i++){
+		ret = pthread_create(&id[i], NULL, (thread_func[i]) , NULL);
 		if(ret == -1){
 			perror("pthread_create");
 			printf("Error  in creating thread %d\n", i);
 			exit(1);
 		} else {
-			printf("Success in creating thread %d\n" i );
+			printf("Success in creating thread %d\n", i );
 		}
 	}
-			
+	
+	for( i = 0; i < NTHREADS ; i++){
+		pthread_join(id[i],&dummy_var);
+	}  
+
+	printf("\n\nMain function is exiting\n\n");
 	return 0;
 }
