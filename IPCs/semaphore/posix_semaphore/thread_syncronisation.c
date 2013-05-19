d11229aef7a7766f3709d5a726ac66c13236d773
Prak <www.prak.co.in@gmail.com>
2013-05-19 11:12:28 +0530

    Added a perfect example of signaling with sem_wait and sem-post

--- IPCs/semaphore/posix_semaphore/thread_syncronisation.c
+++ IPCs/semaphore/posix_semaphore/thread_syncronisation.c
@@ -6,34 +6,34 @@
 #include<semaphore.h>
 
 
-sem_t semid;
+sem_t semid1, semid2;
+int pshared=0;
 int count=0;
-#define MAX 10
+#define MAX 11
 #define SEM_NAME "/test"
 
 void *thread1(void *ptr){
 	int ret;
 	while (1){
-		ret = sem_wait(&semid);
+		ret = sem_wait(&semid1);
 		if(ret == -1){
 			perror("sem_wait");
 			return ;
 		}
 		count++;
 		if(count >= MAX){
-			ret =sem_post(&semid);
+			ret =sem_post(&semid2);
 			if(ret == -1){
 				printf("error on sem wiat function\n");
 			}
 			pthread_exit(&ret);
 		}
 		printf("Count from thread 1 %d\n",count);
-		ret = sem_post(&semid);
+		ret = sem_post(&semid2);
 		if(ret == -1){
 			printf("error on sem post function\n");
 			return 0;
 		}
-		sleep(1);
 	}
 }
 
@@ -42,26 +42,25 @@ void *thread1(void *ptr){
 void *thread2(void*ptr){
 	int ret;
 	while (1){
-		ret = sem_wait(&semid);
+		ret = sem_wait(&semid2);
 		if(ret == -1){
 			printf("error on sem wiat function\n");
 			return ;
 		}
 		count++;
 		if(count >= MAX){
-			ret = sem_post(&semid);
+			ret = sem_post(&semid1);
 			if(ret == -1){
 				printf("error on sem post function\n");
 			}
 			pthread_exit(&ret);
 		}
 		printf("Count from thread 2 %d\n",count);
-		ret =sem_post(&semid);
+		ret =sem_post(&semid1);
 		if(ret == -1){
 			printf("error on sem post function\n");
 			return ;
 		}
-		sleep(1);
 	}
 }
 
@@ -70,10 +69,15 @@ int main(void){
 	int ret;
 	int *exit_code;
 	
-	if((ret = sem_init(&semid,0,1) ) == -1){
+	if((ret = sem_init(&semid1,pshared,1) ) == -1){
 		perror("Error in initializing the semaphore\n");
 		exit(-1);
 	}
+	if((ret = sem_init(&semid2,pshared,0) ) == -1){
+		perror("Error in initializing the semaphore\n");
+		exit(-1);
+	}
+
 	ret = pthread_create(&id1, NULL, &thread1, NULL);
 	ret = pthread_create(&id2, NULL, &thread2, NULL);
 	
@@ -81,7 +85,11 @@ int main(void){
 	pthread_join(id1, (void *)&exit_code);
 	pthread_join(id2, (void *)&exit_code);
 	
-	if((ret = sem_destroy(&semid)) == -1){
+	if((ret = sem_destroy(&semid1)) == -1){
+		perror("Error in destroying the semaphore\n");
+		exit(-1);
+	}
+	if((ret = sem_destroy(&semid2)) == -1){
 		perror("Error in destroying the semaphore\n");
 		exit(-1);
 	}
