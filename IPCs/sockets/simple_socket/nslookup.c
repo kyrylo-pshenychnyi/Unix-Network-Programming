1e5f982ab07ca8f197dda4a62f15678d7ddd815d
Prak <www.prak.co.in@gmail.com>
2013-05-26 11:51:33 +0530

    Added a program  to show the gethostbyname utility

--- IPCs/sockets/simple_socket/nslookup.c
+++ IPCs/sockets/simple_socket/nslookup.c
@@ -3,12 +3,25 @@
 	utility from command line. This is done usin programming
 */
 
+/* 
+	For reference:
+		struct hostent {
+        char    *h_name;        // official name of host 
+        char    **h_aliases;    // alias list 
+        int     h_addrtype;     // host address type 
+        int     h_length;       // length of address 
+        char    **h_addr_list;  // list of addresses 
+	}
+	#define h_addr  h_addr_list[0]  // for backward compatibility 
+
+*/
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<netdb.h>
 #include<arpa/inet.h>
 #include<netinet/in.h>
-
 int main(int argc, char * argv[]){
 	printf("\nProgram to show the usage of gethostbyname() C function\n");
 	printf("=======================================================\n");
@@ -21,14 +34,17 @@ int main(int argc, char * argv[]){
 		printf("Example: ./nslookup www.google.co.in>\n");
 		exit(0);
 	}
-	
+
 	host = gethostbyname(argv[1]);
 	if(host == NULL){
 		perror("gethostbyname error\n");
 		exit(1);
 	} else {
 		h_addr.s_addr = *((unsigned long *) host->h_addr_list[0]);
-		printf("\n%s ->  %s\n\n",argv[1],inet_ntoa(h_addr));
+		printf("\n%s -> IP address  	%s\n",argv[1],inet_ntoa(h_addr));
+		printf("%s -> Length      	%d\n",argv[1],host->h_length);
+		printf("%s -> Address type	%d\n",argv[1],host->h_addrtype);
+		printf("%s -> Name     		%s\n\n",argv[1],host->h_name);
 		return 0;
 	}
 	return 0;
