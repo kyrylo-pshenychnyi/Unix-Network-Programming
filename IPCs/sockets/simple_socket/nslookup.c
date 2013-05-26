e7d2a3550843768b701c7366f01950b43921cd6c
Prak <mailme.prak@gmail.com>
2013-05-26 11:09:50 +0530

    Added a program  to show the gethostbyname utility

---
+++ IPCs/sockets/simple_socket/nslookup.c
@@ -0,0 +1,36 @@
+/* program to get the ip address of a host by its name */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+
+
+
+int main(int argc, char * argv[]){
+	printf("\nProgram to show the usage of gethostbyname() C function\n");
+	printf("=======================================================\n");
+	struct hostent *host;
+	struct in_addr h_addr;
+	int ret ;
+	
+	if(argc < 2){
+		printf("Usage: ./nslookup <hostname>\n");
+		printf("Example: ./nslookup www.google.co.in>\n");
+		exit(0);
+	}
+	
+	host = gethostbyname(argv[1]);
+	if(host == NULL){
+		perror("gethostbyname error\n");
+		exit(1);
+	} else {
+		h_addr.s_addr = *((unsigned long *) host->h_addr_list[0]);
+		printf("\n%s ->  %s\n\n",argv[1],inet_ntoa(h_addr));
+		return 0;
+	}
+	return 0;
+}
+	
+	
