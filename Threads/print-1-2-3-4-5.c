d2371fec16f3bdf6aa63272f152e424402766fc2
Prak <www.prak.co.in@gmail.com>
2013-06-10 08:50:14 +0530

    Synchronisation by mutex method

--- Threads/print-1-2-3-4-5.c
+++ Threads/print-1-2-3-4-5.c
@@ -1,61 +1,97 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
-
-#define MAX 10
-pthread_mutex_t mutex_count=PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond_mutex= PTHREAD_COND_INITIALIZER;
-
-int count = 0;
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<semaphore.h>
 
 
-void * thread1(void *ptr){
-	while(1){
-		pthread_mutex_lock(&mutex_count);
-		if((count%2) != 0){
-			pthread_cond_wait(&cond_mutex,&mutex_count);
-		}
-		count++;
-		printf("%d\n", count);
-		pthread_cond_signal(&cond_mutex);
-		if(count >= MAX){
-			pthread_mutex_unlock(&mutex_count);
-			return;
-		}
-		pthread_mutex_unlock(&mutex_count);
-	}
-	return ;
-}
+sem_t semid1, semid2;
+int pshared=0;
+int count=0;
+#define MAX 11
+#define SEM_NAME "/test"
 
-
-void *thread2(void* ptr){
-	while(1){
-		pthread_mutex_lock(&mutex_count);
-		if(count%1 != 0){
-			pthread_cond_wait(&cond_mutex,&mutex_count);
-		}
-		count++;
-		printf("%d\n", count);
-		pthread_cond_signal(&cond_mutex);
-		if(count >= MAX){
-			pthread_mutex_unlock(&mutex_count);
+void *thread1(void *ptr){
+	int ret;
+	while (1){
+		ret = sem_wait(&semid1);
+		if(ret == -1){
+			perror("sem_wait");
 			return ;
 		}
-		pthread_mutex_unlock(&mutex_count);
+		count++;
+		if(count >= MAX){
+			ret =sem_post(&semid2);
+			if(ret == -1){
+				printf("error on sem wiat function\n");
+			}
+			pthread_exit(&ret);
+		}
+		printf("Count from thread 1 %d\n",count);
+		ret = sem_post(&semid2);
+		if(ret == -1){
+			printf("error on sem post function\n");
+			return 0;
+		}
 	}
-return ;
 }
 
+
+
+void *thread2(void*ptr){
+	int ret;
+	while (1){
+		ret = sem_wait(&semid2);
+		if(ret == -1){
+			printf("error on sem wiat function\n");
+			return ;
+		}
+		count++;
+		if(count >= MAX){
+			ret = sem_post(&semid1);
+			if(ret == -1){
+				printf("error on sem post function\n");
+			}
+			pthread_exit(&ret);
+		}
+		printf("Count from thread 2 %d\n",count);
+		ret =sem_post(&semid1);
+		if(ret == -1){
+			printf("error on sem post function\n");
+			return ;
+		}
+	}
+}
 
 int main(void){
-	pthread_t pid1, pid2;
-	void *ptr;
+	pthread_t id1,id2;
 	int ret;
-	ret = pthread_create(&pid1,NULL,&thread1,NULL);
-	ret = pthread_create(&pid2,NULL,&thread2,NULL);
+	int *exit_code;
+	
+	if((ret = sem_init(&semid1,pshared,1) ) == -1){
+		perror("Error in initializing the semaphore\n");
+		exit(-1);
+	}
+	if((ret = sem_init(&semid2,pshared,0) ) == -1){
+		perror("Error in initializing the semaphore\n");
+		exit(-1);
+	}
 
-	pthread_join(pid1,&ptr);
+	ret = pthread_create(&id1, NULL, &thread1, NULL);
+	ret = pthread_create(&id2, NULL, &thread2, NULL);
+	
+	
+	pthread_join(id1, (void *)&exit_code);
+	pthread_join(id2, (void *)&exit_code);
+	
+	if((ret = sem_destroy(&semid1)) == -1){
+		perror("Error in destroying the semaphore\n");
+		exit(-1);
+	}
+	if((ret = sem_destroy(&semid2)) == -1){
+		perror("Error in destroying the semaphore\n");
+		exit(-1);
+	}
 	return 0;
 }
-
-
