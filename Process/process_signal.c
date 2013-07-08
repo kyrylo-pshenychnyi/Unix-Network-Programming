90a2a0f33d818b4046649789a3d4cfafbe770bb3
prakash <www.prak.co.in@gmail.com>
2013-07-08 18:41:26 +0530

    Create process_signal.c
    A process which will take synchronise parent and child process.

---
+++ Process/process_signal.c
@@ -0,0 +1,152 @@
+#define _XOPEN_SOURCE 600
+
+#include <limits.h> /* For LONG_MAX */
+#include <fcntl.h> /*For open()*/
+#include <unistd.h> /*For close(), read(), write(), nice()*/
+#include <stdlib.h> /*For exit()*/
+#include <stdio.h> /*For fprintf(),perror()*/
+#include <signal.h> /*For signal()*/
+#include <sys/types.h> /* For pid_t */
+#include <sys/wait.h> /* For wait() */
+#include <string.h> /* For wait() */
+
+static volatile sig_atomic_t sigusr1 = 0;
+
+#define BUF_SIZE 200
+void my_handler(int);
+void displayUsage(FILE *, const char *);
+
+int main(int argc, char **argv)
+{
+    char c[BUFSIZ]; /* To hold the string that has been read from the file */
+    int fd; /* file descriptor */
+    int n;
+    long int nBytes; /* To hold the number of bytes to read */
+    pid_t pid;
+    int child_status;
+    char buff[BUF_SIZE];
+    /* Display usage if user use this process wrong */
+    if(argc != 3){
+        displayUsage(stderr, argv[0]);
+        exit(1);
+    }
+
+    /* Convert argv[1] to long - number of bytes to read */
+    nBytes = strtol(argv[1], NULL, 10);
+    if((nBytes <= 0) || (nBytes > INT_MAX)){
+        fprintf(stderr, "Bufferize %s is not a positive integer\n",
+                argv[1]);
+        exit(2);
+    }
+
+    /* Open a file */
+    if((fd = open(argv[2],O_RDONLY,0)) < 0){
+        perror(argv[2]);
+        exit(3);
+    }
+
+    /* Attempt to register a signal handler */
+    if(signal(SIGUSR1, my_handler) == SIG_ERR){
+        perror("Could not set a handler for SIGUSR1");
+        exit(4);
+    }
+
+    /* lowering the process priority */
+    if(nice(40) < 0){
+        perror("Cannot lower the priority");
+        exit(5);
+    }
+
+    /* Create a new process */
+    pid = fork();
+    if(pid < 0){
+        perror("fork");
+        exit(6);
+    }
+    /* Allocating memory space for string/data to be read from the file
+       if(!(c = malloc(nBytes))){
+       perror("ERROR");
+       exit(4);
+       }*/
+
+    if(pid == 0){
+        #if 0
+        sprintf(buff,"Child (pid = %d) about to signal self (sigusr1 = %d)\n", (int)getpid(), (int)sigusr1);
+        write(1, buff, strlen(buff));
+        fflush(0);
+        kill(pid, SIGUSR1);
+        sprintf(buff,"Child (%d) about to pause() (sigusr1 = %d)\n", (int)getpid(), (int)sigusr1);
+        write(1, buff, strlen(buff));
+        fflush(0);
+        #endif 
+
+        pause();
+
+        /* I'm the child */
+        printf("Child's turn %d!\n", (int)getpid());
+        lseek(fd, 0L, SEEK_SET);
+        /* Read The File */
+        while((n = read(fd, c, nBytes)) != 0){
+            /* Write content of 'c' to the stdout */
+            if(write(STDOUT_FILENO, c, n) < 0){
+                perror("Write Error!\n");
+                exit(7);
+            }
+        }
+        printf("Child signalling parent\n");
+        kill(pid,SIGUSR1);
+        printf("Child exiting (status = 37)\n");
+        exit(37);
+    }
+    else{
+        /* I'm the parent */
+        sprintf(buff,"Parent's turn! (pid = %d, kid = %d)\n", (int)getpid(), (int)pid);
+        write(1, buff, strlen(buff));
+        /* Read The File */
+        while((n = read(fd, c, nBytes)) != 0){
+            /* Write content of 'c' to the stdout */
+            if(write(STDOUT_FILENO, c, n) < 0){
+                perror("Write Error!\n");
+                exit(7);
+            }
+        }
+        kill(pid, SIGUSR1);
+        /* Reap the child */
+        int corpse = wait(&child_status);
+        printf("waiting over: pid = %d, status = 0x%.4X\n", corpse, child_status);
+    }
+
+    /* Close the file */
+    if(close(fd) < 0){
+        perror(argv[2]);
+        exit(8);
+    } else {
+        printf("close fd in %d\n", (int)getpid());
+    }
+
+    printf("%d exiting\n", (int)getpid());
+    return(0);
+}
+
+void displayUsage(FILE *fp, const char *arg)
+{
+        fprintf(fp, "Usage: %s n filename\n", arg);
+        fprintf(fp, "where\n");
+        fprintf(fp, 
+            "\tn\tNumber of bytes to read and write in each \"block\".\n");
+        fprintf(fp,"\tfilename\tName of file to be catted.\n");
+}
+
+void my_handler(int sig)
+{
+    /* Re-registering the handler */
+    if(signal(sig, my_handler) == SIG_ERR){
+        perror("Could not set a handler for SIGUSR1");
+        exit(4);
+    }
+    sigusr1 = 1;
+    #if 0
+    printf("In signal handler (pid %d)\n", (int)getpid());
+    fflush(0);
+    #endif
+}
