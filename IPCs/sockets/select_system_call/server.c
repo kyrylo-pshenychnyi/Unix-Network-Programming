490950fa63a128f45758fb10e21d08a0833b3851
prakash <www.prak.co.in@gmail.com>
2013-07-15 18:57:41 +0530

    Update server.c

--- IPCs/sockets/select_system_call/server.c
+++ IPCs/sockets/select_system_call/server.c
@@ -5,21 +5,25 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/socket.h>
+#include<arpa/inet.h>
+
 
 
 #define MAX_FD 10
 #define BUFF_SIZE 200
 #define DEFAULT_PORT 23456
+
 int main( int argc, char * argv[]){
     int ret;
     int socketfd,connectfd;
     int serv_port;
     int client[FD_SETSIZE];
-    struct sock_addr_in server_addr, client_addr;
-    int i;
+    char buf[BUFF_SIZE];
+    struct sockaddr_in server_addr, client_addr;
+    int i, max_fd;
     fd_set allset, rset;
 
-    socketfd =  socket(AF_INET, SOCK_STREAM, 0);
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
     if(socketfd == -1){
         perror("Socket\n");
         exit(1);
@@ -34,49 +38,77 @@ int main( int argc, char * argv[]){
     bzero(&server_addr,sizeof(server_addr));
     bzero(&client_addr,sizeof(client_addr));
 
-    server_addr.sin_faimily     = AF_INET;
-    server_addr.sin_port        = htons(serv_port);
-    server_addr.sin_addr.s_addr = hton(INADDR_ANY)
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(serv_port);
+    server_addr.sin_addr.s_addr = htons(INADDR_ANY);
     
     ret = bind(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     if(ret == -1){
         perror("Bind\n");
         exit(2);
-    } 
+    }
 
     ret = listen(socketfd, MAX_FD);
     if(ret == -1){
-        perror("listen\n"); 
+        perror("listen\n");
         exit(3);
     }
 
-    max_fd = socketfd;
+    printf("The value of FD_SETSIZE = %d\n",FD_SETSIZE);
+    printf("The value of socketfd = %d\n",socketfd);
     for(i = 0; i< FD_SETSIZE ; i++){
         client[i] = -1;
     }
     FD_ZERO(&allset);
     FD_SET(socketfd, &allset);
-    printf("The value of FD_SETSIZE = %d\n", sizeof(FD_SETSIZE));
 
-    int ready;
-    
+    max_fd = socketfd;
+    int maxi = -1;
+    int nready, n;
     for (; ; ){
         rset = allset;
-        ready = select(max_fd + 1, &rset , NULL ,NULL, NULL); 
-        if(ready == -1){
+        nready = select(max_fd + 1, &rset , NULL ,NULL, NULL);
+        if(nready == -1){
             perror("Select\n");
             exit(3);
+        } else {
+            printf("Number of ready discriptor = %d\n", nready);
         }
+
+#if 1
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        for(a = 0; a < FD_SETSIZE; a++){
+            if(FD_ISSET(a,&rset)){
+                printf("1 ");
+            } else {
+                printf("0 ");   
+            }
+            b++;
+            if(b == 16){
+                printf("\n");
+                b = 0;
+                break; // Not expecting more than 16 client request at a time 
+            }
+        }
+#endif
         if(FD_ISSET(socketfd, &rset)){
             int clilen = sizeof(client_addr);
-            connectfd =  accept(socketfd,
-                                (struct sock_addr*)&(client_addr), 
+            connectfd = accept(socketfd,
+                               (struct sockaddr*)&(client_addr),
                                 &clilen);
-            printf("new client: %s, port %d\n",
-                    Inet_ntop(AF_INET, &cliaddr.sin_addr, 4, NULL),
-                    ntohs(cliaddr.sin_port));
-            
-            for(i = 0; i< FD_SETSIZE, i++){
+            if( connectfd == -1){
+                perror("Accept:\n");
+                exit(7);
+            } else {
+#if 1
+                char str[100];
+                printf("new client: %s, port %hu => Client fd - %d\n",
+                    inet_ntop(AF_INET, (struct sockaddr *) &client_addr.sin_addr, str, 100),ntohs(client_addr.sin_port), connectfd);
+#endif
+            }
+            for(i = 3; i < FD_SETSIZE; i++){
                 if(client[i] < 0){
                     client[i] = connectfd;
                     break;
@@ -91,35 +123,34 @@ int main( int argc, char * argv[]){
             if(connectfd > max_fd){
                 max_fd = connectfd;
             }
-            if(i > max_fd){
-                max_fd = 1;
+            if(i > maxi){
+                maxi = i;
             }
             
-            if( --neready <= 0){
+            if( --nready <= 0){
                 continue;
             }
-        } 
-        for (i = 0; i <= maxi; i++) {   /* check all clients for data */
-            if ( (sockfd = client[i]) < 0)
+        }
+        for (i = 0; i <= max_fd ; i++) { /* check all clients for data */
+            if ( (socketfd = client[i]) < 0)
                 continue;
-            if (FD_ISSET(sockfd, &rset)) {
-                if ( (n = Read(sockfd, buf, MAXLINE)) == 0) {
+            if (FD_ISSET(socketfd, &rset)) {
+                if ( (n = read(socketfd, buf, BUFF_SIZE)) == 0) {
                         /*4connection closed by client */
-                    Close(sockfd);
-                    FD_CLR(sockfd, &allset);
+                    close(socketfd);
+                    FD_CLR(socketfd, &allset);
                     client[i] = -1;
                 } else
-                    Writen(sockfd, buf, n);
+                    printf("Data read from client [ %s ] with socket %d\n", buf, socketfd); 
+                    if(write(socketfd, buf, n) == -1){
+                        perror("Write:\n");
+                        exit(6);
+                } 
 
                 if (--nready <= 0)
-                    break;              /* no more readable descriptors */
+                    break; /* no more readable descriptors */
             }
         }
     }
     return 0;
 }
-
-
-
-
-
